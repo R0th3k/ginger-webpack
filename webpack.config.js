b567e0d446e01ff5ddbdaf7dd13bd6f5ac2f450d
Hektor Peña <dev@hektor.mx>
2019-11-19 23:32:47 -0600

    Servidor de pruebas

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
-  }
+  }, 
+  devServer:{
+    port: 7777
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
