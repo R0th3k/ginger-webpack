120f1a33c6fe8d885b0c20722356c329d27bade8
Hektor Peña <dev@hektor.mx>
2019-11-20 00:56:41 -0600

    Componente .vue de ejemplo

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <h1 class="title">{{title}}</h1>
+    <h1>{{title}}</h1>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
 <script>
+import HelloWorld from './HelloWorld.vue';
 export default{
+  name: 'App',
   data(){
     return {
       title: 'Hola Mundo'
     }
+  },
+  components:{
+    HelloWorld
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.title{
+<style lang="scss">
+h1{
   color:green;
 }
 </style>
---
+++ src/js/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'HelloWorld',
+    data(){
+      return{
+        title:'Hello World from component .vue'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+h1{
+  color:blue;
+}
+</style>
