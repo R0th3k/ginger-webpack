812ebb44e4cda83d869f2ff75077399e8833b1e2
Hektor Peña <dev@hektor.mx>
2019-11-19 23:52:38 -0600

    Extraer el css y mejora en tareas de npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,13 +14,16 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/i,
-        use: ['style-loader', 'css-loader','sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
       },
     ],
   },
   plugins:[
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.min.css'
     })
   ]
 }
