b868a92f8979853ace92f671695d52e74c4b1431
Hektor Peña <dev@hektor.mx>
2019-11-20 09:50:44 -0600

    Código Runtime

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,12 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
   plugins:[
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
